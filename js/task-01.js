a68610536e0bddc297e52c709f9795a4051d8d2a
Max Rohivskyi <maxrogivsky@gmail.com>
2022-09-02 00:23:25 +0300

    add-update-task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,45 @@
-const numberOfСategories = document.querySelectorAll('.item');
+// 1
+// const numberOfСategories = document.querySelectorAll('.item');
 
-console.log(`Number of categories: ${numberOfСategories.length}`);
-
-
-const numberOfСategoriesArr = [...numberOfСategories]
-    .map(category => `Category: ${category.children[0].textContent} Elements: ${category.children[1].children.length}`)
-    .join('\n');
-
-console.log(numberOfСategoriesArr);
+// console.log(`Number of categories: ${numberOfСategories.length}`);
 
 
+// const numberOfСategoriesArr = [...numberOfСategories]
+//     .map(category => `Category: ${category.children[0].textContent} Elements: ${category.children[1].children.length}`)
+//     .join('\n');
+
+// 2
+// console.log(numberOfСategoriesArr);
+
+// const categoriesEl = document.querySelector('#categories')
+
+// const quantityCategories = param => {
+//   const items = param.children.length;
+//   return `В списке ${items} категории.`
+// }
+
+// console.log(quantityCategories(categoriesEl));
+
+
+// const items = document.querySelectorAll('.item')
+
+// items.forEach(item => {
+//   const titleEl = item.firstElementChild
+//   const quantityElement = item.lastElementChild.childElementCount
+//   console.log(`Категория: ${titleEl.textContent}, Количество элементов: ${quantityElement}`);
+// })
+
+// 3
+// const numberOfCategories = document.querySelectorAll('.item');
+
+// function results() {
+//     console.log('Number of categories:', numberOfCategories.length);
+//     numberOfCategories.forEach(numberOfCategorie => {
+//         const sumEl = numberOfCategorie.lastElementChild.childElementCount
+//         const categoryEl = numberOfCategorie.firstElementChild
+        
+//     console.log('Category:', categoryEl.textContent, `Elements: ${sumEl}`);
+// });
+// }
+
+// results()
